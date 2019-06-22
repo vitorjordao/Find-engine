3dc404be2043ede9e81f77dc03a17aef182f0af0
Vitor Jordão <srvitorlol@gmail.com>
2019-06-22 13:54:34 -0300

    Initial commit

---
+++ js/controller/FinderController.js
@@ -0,0 +1,14 @@
+class FinderController{
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._pages = $("#searched-pages");
+    }
+
+    importPages(){
+        _cleanPages();
+    }
+
+    _cleanPages(){
+
+    }
+}
